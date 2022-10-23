bbf23224f97bbaaf230564f72383b96cd5e61912
Gia Nguyên Ngô <ngokhoaisan1999@gmail.com>
2022-10-23 23:27:16 +0700

    Add example unit test by jest (#29)

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,12 @@ module.exports =  {
   entry: './src/index.ts',
   devtool: 'inline-source-map',
   module: {
+    loaders: [
+      {exclude: ['node_modules'], loader: 'babel', test: /\.jsx?$/},
+      {loader: 'style-loader!css-loader', test: /\.css$/},
+      {loader: 'url-loader', test: /\.gif$/},
+      {loader: 'file-loader', test: /\.(ttf|eot|svg)$/},
+    ],
     rules: [
       {
         test: /\.tsx?$/,
@@ -18,7 +24,17 @@ module.exports =  {
     // compress: true,
   },
   resolve: {
+    alias: {
+      config$: './configs/app-config.js',
+      react: './vendor/react-master',
+    },
     extensions: ['.tsx', '.ts', '.js'],
+    modules: [
+      'node_modules',
+      'bower_components',
+      'shared',
+      '/shared/vendor/modules',
+    ],
   },
   output: {
     filename: 'bundle.js',
